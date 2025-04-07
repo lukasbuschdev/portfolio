f8f26d5e42dc58b6478f258f71ebe862956b4bf0
lukasbuschdev <buschlukas175@gmail.com>
2025-04-07 00:35:19 -0600

    fixed/implemented projects section

--- src/app/main/projects/projects.component.ts
+++ src/app/main/projects/projects.component.ts
@@ -8,7 +8,21 @@ import { LanguageService } from '../../services/language.service';
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  activeCardId: string  = '';
+  
   language = inject(LanguageService);
+
+  toggleActiveCard(event: MouseEvent, id: string): void {
+    event.stopPropagation();
+    console.log(id)
+    if(this.activeCardId === id) return this.closeCard();
+    this.activeCardId = id;
+    console.log(this.activeCardId)
+  }
+
+  closeCard(): void {
+    this.activeCardId= '';
+  }
 
   openLink(link: string): void {
     window.open(link, '_blank');
@@ -18,33 +32,37 @@ export class ProjectsComponent {
     const translations: any = {
       en: [
         {
+          id: '0',
           name: 'Expense Tracker',
           technologies: 'Angular 19 | TypeScript | HTML | SCSS | Supabase | Node | Express',
-          description: 'Expense Tracker is a full-stack application that simplifies financial management. Create an account to unlock a dynamic dashboard with graphical summaries, manage your expenses, budgets, and categories, and adjust your settings all in one intuitive interface.',
+          description: 'Expense Tracker is a full-stack financial management app. Create an account to access a dynamic dashboard that lets you manage expenses, budgets, and settings in one interface.',
           img: 'img/expensetracker.png',
           link: 'https://expensetracker.lukasbusch.dev',
           github: 'https://github.com/lukasbuschdev/tracker'
         },
         {
+          id: '1',
           name: 'Join',
           technologies: 'JavaScript | HTML | CSS | Firebase',
-          description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+          description: 'Task Manager, inspired by the Kanban system, lets you create and organize tasks with drag-and-drop ease. Assign users and categories to manage your workflow and improve task tracking.',
           img: 'img/join.png',
           link: 'https://lukasbusch.dev/Join/init/login/login.html',
           github: 'https://github.com/lukasbuschdev/Join'
         },
         {
+          id: '2',
           name: 'Pollo Loco',
           technologies: 'JavaScript | HTML | CSS',
-          description: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
+          description: 'Jump, run, and throw game built with an object-oriented approach. Help Pepe find coins and tabasco salsa to battle the crazy hen in an exciting quest.',
           img: 'img/polloloco.png',
           link: 'https://lukasbusch.dev/PolloLoco/index.html',
           github: 'https://github.com/lukasbuschdev/ElPolloLoco'
         },
         {
+          id: '3',
           name: 'Notes',
           technologies: 'JavaScript | HTML | CSS | Firebase',
-          description: 'Create, edit, archive, and delete notes effortlessly with inbuilt search functionality. Available in 5 languages and downloadable for any device.',
+          description: 'Create, edit, archive, and delete notes effortlessly with inbuilt search functionality. Available in 5 languages and available as PWA.',
           img: 'img/notes.png',
           link: 'https://lukasbusch.dev/Notes/index.html?fullscreen=true',
           github: 'https://github.com/lukasbuschdev/Notes'
@@ -52,6 +70,7 @@ export class ProjectsComponent {
       ],
       es: [
         {
+          id: '0',
           name: 'Expense Tracker',
           technologies: 'Angular 19 | TypeScript | HTML | SCSS | Supabase | Node | Express',
           description: 'Expense Tracker es una aplicación full-stack que simplifica la gestión financiera. Crea una cuenta para acceder a un panel dinámico con resúmenes gráficos, gestiona tus gastos, presupuestos y categorías, y ajusta tu configuración, todo en una interfaz intuitiva.',
@@ -60,6 +79,7 @@ export class ProjectsComponent {
           github: 'https://github.com/lukasbuschdev/tracker'
         },
         {
+          id: '1',
           name: 'Join',
           technologies: 'JavaScript | HTML | CSS | Firebase',
           description: 'Administrador de tareas inspirado en el sistema Kanban. Crea y organiza tareas usando funciones de arrastrar y soltar, asignar usuarios y categorías.',
@@ -68,6 +88,7 @@ export class ProjectsComponent {
           github: 'https://github.com/lukasbuschdev/Join'
         },
         {
+          id: '2',
           name: 'Pollo Loco',
           technologies: 'JavaScript | HTML | CSS',
           description: 'Juego de saltar, correr y lanzar basado en un enfoque orientado a objetos. Ayuda a Pepe a encontrar monedas y salsa tabasco para luchar contra la gallina loca.',
@@ -76,6 +97,7 @@ export class ProjectsComponent {
           github: 'https://github.com/lukasbuschdev/ElPolloLoco'
         },
         {
+          id: '3',
           name: 'Notas',
           technologies: 'JavaScript | HTML | CSS | Firebase',
           description: 'Crea, edita, archiva y elimina notas fácilmente con funcionalidad de búsqueda incorporada. Disponible en 5 idiomas y descargable para cualquier dispositivo.',
@@ -86,6 +108,7 @@ export class ProjectsComponent {
       ],
       de: [
         {
+          id: '0',
           name: 'Expense Tracker',
           technologies: 'Angular 19 | TypeScript | HTML | SCSS | Supabase | Node | Express',
           description: 'Expense Tracker ist eine Full-Stack-Anwendung, die das Finanzmanagement vereinfacht. Erstelle ein Konto, um ein dynamisches Dashboard mit grafischen Übersichten freizuschalten, verwalte deine Ausgaben, Budgets und Kategorien und passe deine Einstellungen in einer intuitiven Benutzeroberfläche an.',
@@ -94,6 +117,7 @@ export class ProjectsComponent {
           github: 'https://github.com/lukasbuschdev/tracker'
         },
         {
+          id: '1',
           name: 'Join',
           technologies: 'JavaScript | HTML | CSS | Firebase',
           description: 'Task-Manager inspiriert vom Kanban-System. Erstellen und organisieren Sie Aufgaben mit Drag-and-Drop-Funktionen, weisen Sie Benutzern und Kategorien zu.',
@@ -102,6 +126,7 @@ export class ProjectsComponent {
           github: 'https://github.com/lukasbuschdev/Join'
         },
         {
+          id: '2',
           name: 'Pollo Loco',
           technologies: 'JavaScript | HTML | CSS',
           description: 'Spring-, Lauf- und Wurfspiel basierend auf einem objektorientierten Ansatz. Hilf Pepe, Münzen und Tabascosauce zu finden, um gegen die verrückte Henne zu kämpfen.',
@@ -110,6 +135,7 @@ export class ProjectsComponent {
           github: 'https://github.com/lukasbuschdev/ElPolloLoco'
         },
         {
+          id: '3',
           name: 'Notizen',
           technologies: 'JavaScript | HTML | CSS | Firebase',
           description: 'Erstellen, bearbeiten, archivieren und löschen Sie Notizen mühelos mit eingebauter Suchfunktionalität. In 5 Sprachen verfügbar und für jedes Gerät herunterladbar.',
